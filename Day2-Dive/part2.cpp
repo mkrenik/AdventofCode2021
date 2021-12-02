c839df8d7c241c433e796703baf269a6194e36aa
Matthew Krenik <m-krenik@hotmail.com>
2021-12-02 15:51:02 -0600

    day 2 is done

---
+++ Day2-Dive/part2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>     // std::cout, std::endl
+#include <fstream>      // std::fstream
+#include <string>       // std::string
+#include <sstream>      // std::stringstream
+
+int main() 
+{
+  std::fstream f("input", std::ios_base::in);
+
+  int currentAim = 0; 
+  int currentDepth = 0;
+  int currentHorizontal = 0; 
+  std::string tempLine;
+  bool firstLine = true; 
+
+  if(f){
+    while(getline(f, tempLine)){  // gets the whole line
+      std::stringstream tempStream(tempLine); 
+
+      std::string tempInstr; 
+      int tempDistance = 0;  
+
+      getline(tempStream, tempInstr, ' '); // splits up the rest by space
+      tempStream >> tempDistance; 
+
+      // std::cout << "tempInstr: " << tempInstr  << std::endl;
+      // std::cout << "tempDistance: " << tempDistance  << std::endl;
+
+      if (tempInstr == "forward") {
+        currentHorizontal += tempDistance; 
+        currentDepth += currentAim * tempDistance; 
+      } else if (tempInstr == "up") {
+        currentAim -= tempDistance; 
+      } else if (tempInstr == "down") {
+        currentAim += tempDistance; 
+      } else {
+        std::cout << "Error: unable to parse instruction" << std::endl;
+      }
+    }
+  } else{
+    std::cout << "Error: couldn't open file" << std::endl;
+  }
+
+  f.close();
+
+  std::cout << "Current Depth: " << currentDepth << std::endl;
+  std::cout << "Current Horizontal: " << currentHorizontal << std::endl;
+  std::cout << "Product: " << currentDepth * currentHorizontal << std::endl;
+}

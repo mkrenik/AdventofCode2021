72423fef1a273c949984039d72c9557753c0b0c8
Matthew Krenik <m-krenik@hotmail.com>
2021-12-06 07:08:40 -0600

    refactored to use fishbuckets. finished with part 2

--- Day6-Lanternfish/main.cpp
+++ Day6-Lanternfish/main.cpp
@@ -1,51 +1,82 @@
 #include <iostream>
-#include <vector> 
+#include <deque> 
 #include <sstream>     
 #include <fstream>
 
 #define FILE_NAME "input"
 
-struct Fish {
-  int days; 
-  Fish() : days{8} {}; 
-  Fish(int x) : days{x} {};
+struct FishBuckets {
+  long int zero = 0;
+  long int one = 0; 
+  long int two = 0; 
+  long int three = 0; 
+  long int four = 0; 
+  long int five = 0;
+  long int six = 0; 
+  long int seven = 0; 
+  long int eight = 0; 
+
+  void addInput(int x) {
+    if (x == 0) zero++; 
+    else if (x==1) one++;
+    else if (x==2) two++; 
+    else if (x==3) three++; 
+    else if (x==4) four++; 
+    else if (x==5) five++; 
+    else if (x==6) six++; 
+    else if (x==7) seven++; 
+    else if (x==8) eight++; 
+    else {
+      std::cout << "ERROR" << std::endl; 
+    }
+  }
+
+  long long int count() {
+    return zero + one + two + three + four + five + six + seven + eight; 
+  }
 };
 
+FishBuckets ageByDay(FishBuckets input) {
+  FishBuckets output; 
 
-std::vector<Fish> readInput() {
+  output.zero = input.one; 
+  output.one = input.two; 
+  output.two = input.three; 
+  output.three = input.four;
+  output.four = input.five;
+  output.five = input.six;
+  output.six = input.zero + input.seven; 
+  output.seven = input.eight; 
+  output.eight = input.zero; 
 
-  std::vector<Fish> inputs; 
+  return output; 
+}
+
+
+FishBuckets readInput() {
+
+  FishBuckets school;
   std::fstream f(FILE_NAME, std::ios_base::in);
 
   if (f) {
     std::string temp;
     while (getline(f, temp, ',')) {
-      inputs.push_back(Fish(stoi(temp)));
+      school.addInput(stoi(temp));
     }
   } else {
     std::cout << "Error: couldn't open file" << std::endl;
   }
 
-  return inputs; 
+  return school; 
 }
 
 int main () {
-  std::vector<Fish> school; 
+  FishBuckets school; 
   school = readInput(); 
 
-  for (int i =1 ; i<=80; i++) {
-    std::vector<Fish> newFish; 
-    for (auto &fish : school) {
-      fish.days--; 
-      if (fish.days < 0) {
-        fish.days = 6; 
-        newFish.push_back(Fish()); 
-      }
-    }
-    for (auto &fish : newFish) {
-      school.push_back(fish); 
-    }
-    newFish.clear(); 
-    std::cout << "Day #" << i << ": " << school.size() << " fish" << std::endl; 
+  for (int i =1 ; i<=256; i++) {
+    FishBuckets newSchool = ageByDay(school); 
+    school = newSchool; 
+    std::cout << "Day #" << i << ": " << school.count() << " fish" << std::endl; 
   }
 }

0ff26e2408637cb1c7ea4f1c2fe6898b53663027
Matthew Krenik <m-krenik@hotmail.com>
2021-12-23 20:59:05 -0600

    works with recursion, but still way too slow...

--- Day14-ExtendedPolymerization/main.cpp
+++ Day14-ExtendedPolymerization/main.cpp
@@ -6,12 +6,35 @@
 #include <sstream> 
 #include <climits>
 
+#define GEN 40
 
-struct Result5Gen {
-  std::string resultStr;
+bool first = true; 
 
-  std::map<char, int> resultCounts;  
-};
+void addPolyAndCount(std::string duplex, std::map<std::string, std::string> &table, std::map<char, int> &count, int generations) {
+  if (generations == GEN) {
+    // add to count and return 
+    if (first) {    // There is a bug here... needs to be reset everytime the initial value slides
+      if (count.contains(duplex[0])) {
+        count[duplex[0]]++; 
+      } else {
+        count.insert(std::pair<char, int>(duplex[0], 1)); 
+      }
+      first = false; 
+    }
+    if (count.contains(duplex[1])) {
+      count[duplex[1]]++; 
+    } else {
+      count.insert(std::pair<char, int>(duplex[1], 1)); 
+    }
+    
+  } else {
+    // recurse
+    std::string addChar = table[duplex];
+    // std::cout << "duplex: " << duplex << "   addChar: " << addChar << std::endl; 
+    addPolyAndCount(duplex[0]+addChar, table, count, generations+1);
+    addPolyAndCount(addChar+duplex[1], table, count, generations+1);
+  }
+}
 
 int main(int argc, char *argv[]) {
 
@@ -53,111 +76,29 @@ int main(int argc, char *argv[]) {
   //   it++;
   // }
 
+  std::string twoElementIn; 
+  std::map<char, int> finalCount; 
 
+  for(int i=0; i<polyTemplate.length()-1; i++) {
+    std::cout << "Sliding " << i << std::endl; 
+    twoElementIn = polyTemplate.substr(i, 2);
+    addPolyAndCount(twoElementIn, pairRules, finalCount, 0);   
+  }
 
-  // Run the polymerization on each dictionary entry to accelerate 5 steps
-  std::map<std::string, std::string>::iterator it = pairRules.begin();
-
-  std::map<std::string, Result5Gen> accelerator; 
-
-  std::string input; 
-  while (it != pairRules.end()) {
-    input = it->first;
-    // std::cout << "after 0 steps, is: " << input << std::endl; 
-    for (int step=1; step<=7; step++) {
-      std::stringstream afterStep;  
-      std::string twoElementIn; 
-      std::string toInsert; 
-      for(int i=0; i<input.length()-1; i++) {
-        twoElementIn = input.substr(i, 2);
-        // std::cout << "twoElem: " << twoElementIn << std::endl; 
-        toInsert = pairRules[twoElementIn]; 
-        // std::cout << "toIns: " << toInsert << std::endl; 
-
-        if (i==0) { // first step, insert the first char
-          afterStep << twoElementIn[0]; 
-        }
-        afterStep << toInsert << twoElementIn[1]; 
-      }
-
-      input = afterStep.str(); 
-      // std::cout << "after " << step << " steps, is: " << input << std::endl; 
+  int min=INT_MAX, max=0; 
+  std::map<char, int>::iterator count_it = finalCount.begin();
+  while (count_it != finalCount.end()) {  
+    std::cout << "\t" << count_it->first << ": " << count_it->second; 
+    if(count_it->second > max) {
+      max = count_it->second; 
     }
-
-    std::map<char, int> charCount; 
-    for (int i =0; i<input.size(); i++) {
-      if (charCount.contains(input[i])) {
-        charCount[input[i]]++; 
-      } else {
-        charCount.insert(std::pair<char, int>(input[i], 1)); 
-      }
+    if(count_it->second < min) {
+      min = count_it->second; 
     }
+    count_it++; 
+  }
+  std::cout << std::endl; 
 
-    Result5Gen entry; 
-    entry.resultCounts = charCount; 
-    entry.resultStr = input; 
-    accelerator.insert(std::pair<std::string, Result5Gen>(it->first, entry)); 
-    it++;
-  } 
-
-  // print results of accelerator 
-  std::map<std::string, Result5Gen>::iterator acc_it = accelerator.begin();
-  while (acc_it != accelerator.end()) {
-    std::cout << acc_it->first << ": " << std::endl; 
-    std::cout << "\t" << acc_it->second.resultStr << std::endl; 
-
-    std::map<char, int>::iterator count_it = acc_it->second.resultCounts.begin();
-    while (count_it != acc_it->second.resultCounts.end()) {  
-      std::cout << "\t" << count_it->first << ": " << count_it->second; 
-      count_it++; 
-    }
-    std::cout << std::endl; 
-    acc_it++; 
-  } 
-
-
-  // Use the accelerator  
-  // std::map<char, int> finalCount; 
-
-  // for (int step=1; step<=4; step++) {
-  //   std::stringstream afterStep;  
-  //   std::string twoElementIn; 
-  //   std::string toReplace; 
-  //   for(int i=0; i<polyTemplate.length()-1; i++) {
-  //     twoElementIn = polyTemplate.substr(i, 2);
-  //     // std::cout << "twoElem: " << twoElementIn << std::endl; 
-  //     toReplace = accelerator[twoElementIn].resultStr; 
-  //     // std::cout << "toIns: " << toInsert << std::endl; 
-
-  //     if (i==0) { // first step, insert the first char
-  //       afterStep << twoElementIn[0]; 
-  //     }
-  //     afterStep << toReplace; 
-  //   }
-
-  //   polyTemplate = afterStep.str(); 
-  //   std::cout << "after " << step << " steps, is: " << polyTemplate.length() << std::endl; 
-  // }  
-
-  // Count the characters in the final result 
-
-
-  // // Find the min and the max 
-  // int minCount = INT_MAX;
-  // int maxCount = 0; 
-
-  // std::map<char, int>::iterator it = charCount.begin();
-
-  // while (it != charCount.end()) {
-  //   if (it->second > maxCount) {
-  //     maxCount = it->second;
-  //   }
-  //   if (it->second < minCount) {
-  //     minCount = it->second; 
-  //   }
-  //   it++;
-  // } 
-
-  // std::cout << "Max - min: " << maxCount - minCount << std::endl; 
+  std::cout << "max(" <<max<<") - min("<<min<<"): " << max-min << std::endl; 
 
 }
